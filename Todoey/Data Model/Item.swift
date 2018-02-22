51bfd8be26e7a452bb8e320efdfc271e94cb8401
maxesse <max@maxsanna.com>
2018-02-22 15:15:50 +0000

    Refactored code to use MVC model, and created Item class to store data. Resolved bug with cell reuse and tickmarks.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Max Sanna on 22/02/2018.
+//  Copyright © 2018 Max Sanna. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
